b809e12d8a90af2d407876de03653e3073a95656
Stevenkwizera06 <kwiste06@gmail.com>
2022-11-21 14:55:30 +0200

    worked on reviews

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary_gray: '#f2f2f2',
-        secondary_gray: '#dadce0',
-        primary_dark: '#3c4043',
-        secondary_dark: '#70757a',
-        dark_gray: '#adb2b7',
-        primary_blue: '#4285f4;',
-        primary_dark_blue: '#1a73e8',
-        secondary_dark_blue: '#8ab4f8',
-        secondary_bg_dark:'#0a0a0a',
-        light_gray: '#f8f9fa',
+        "dark-gray": "#adb2b7",
+        "primary-blue": "#4285f4;",
+        "secondary-dark-blue": "#8ab4f8",
       },
       fontFamily: {
-        Roboto: ['Roboto', 'sans-serif'],
+        Roboto: ["Roboto", "sans-serif"],
       },
       fontSize: {
-        xsm: '0.813rem',
+        xsm: "0.813rem",
       },
       boxShadow: {
-        main: '0 1px 6px rgb(32 33 36 / 28%)',
+        main: "0 1px 6px rgb(32 33 36 / 28%)",
       },
     },
   },
   plugins: [],
-}
+};
